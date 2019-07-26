622472ec32e1a9e1559f29652c5570f7a7f29cb6
FelixvL <digitaleservice@gmail.com>
2019-07-26 15:00:58 +0200

    woord scannen lukt

--- LingoApp.java
+++ LingoApp.java
@@ -1,7 +1,31 @@
+
+import java.util.Scanner;
+
 class LingoApp{
 	public static void main(String[] args){
 		System.out.println("Lingo");
-		System.out.println("Lingo2");
+		Poging poging1 = new Poging();
+
+		System.out.println("Het woord is: " + poging1.woord);
+		Scanner sc = new Scanner(System.in);
+		String invoer = sc.next();
+		System.out.println(invoer);
+		System.out.println(poging1.woord.charAt(0));
+		if(poging1.woord.charAt(0) == invoer.charAt(0)){
+			System.out.println("goed");
+		}else{
+			System.out.println("fout");
+		}
+		for(int x = 0 ; x < poging1.woord.length() ; x++){
+			if(invoer.charAt(0) == poging1.woord.charAt(x)){
+				System.out.println("zit erin");
+			}
+		} 
+
 	}
 
+}
+
+class Poging{
+	String woord = "pizza";
 }
